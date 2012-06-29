09ed4659ac582bd821b9f7f5bc95da5e6746970e
scorpion <scorpion.jze@gmail.com>
2012-06-29 18:40:28 -0500

    Partway Implementing MSG_LOGON

---
+++ Taj/Messages/MSG_LOGON.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Taj.Messages
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MSG_LOGON
+    {
+        public MSG_LOGON(int x)
+        {
+            msg = new ClientMsg
+            {
+                eventType = 0x0, //Outgoing msg type for logon
+                length = 128,
+                refNum = 0
+            };
+            rec = new AuxRegistrationRec
+            {
+                crc = 0,
+            };
+        }
+
+        ClientMsg msg;
+        AuxRegistrationRec rec;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Size = 128)]
+    unsafe struct AuxRegistrationRec
+    {
+        public UInt32 crc;
+        public UInt32 counter;
+        public Str31 userName;
+        public Str31 wizPassword;
+        public Int32 auxFlags;
+        public UInt32 puidCtr;
+        public UInt32 CRC;
+        public UInt32 demoElapsed;
+        public UInt32 totalElapsed;
+        public UInt32 demoLimit;
+        public Int16 desiredRoom;
+        public fixed byte reserved[6];
+        public UInt32 ulRequestedProtocolVersion;
+        public UInt32 ulUploadCaps;
+        public UInt32 ulDownloadCaps;
+        public UInt32 ul2DEngineCaps;
+        public UInt32 ul2DGraphicsCaps;
+        public UInt32 ul3DEngineCaps;
+    }
+}
---
+++ Taj/Messages/Str31.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Taj.Messages
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct Str31
+    {
+        public byte length;
+        public fixed sbyte msg[31];
+
+        public string Content
+        {
+            get
+            {
+                fixed (sbyte* pMsg = msg)
+                    return new string(pMsg, 0, length);
+            }
+        }
+    }
+}
