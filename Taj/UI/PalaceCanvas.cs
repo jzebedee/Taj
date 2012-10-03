57665feabb10958e7c83cf3662d78a37ee113d3b
scorpion <scorpion.jze@gmail.com>
2012-10-03 06:09:24 -0500

    PalaceCanvas stub

---
+++ Taj/UI/PalaceCanvas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Taj.UI
+{
+    public partial class PalaceCanvas : UserControl
+    {
+        public PalaceCanvas()
+        {
+            InitializeComponent();
+        }
+    }
+}
