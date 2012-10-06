0d94710275c0eebf35c51b46bed6d1a008dfb6a5
scorpion <scorpion.jze@gmail.com>
2012-10-06 13:37:13 -0500

    Console->WPF. UI stubs.

---
+++ Taj/UI/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Taj.UI
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
