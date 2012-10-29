e24187f0bf2f0af761887509ad75927723faaa76
James Zebedee <scorpion.jze@gmail.com>
2012-10-29 06:46:17 -0500

    Asset manager stubs

---
+++ Taj/Assets/FlatFileManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taj.Assets
+{
+    public class FlatFileManager : IAssetManager
+    {
+        public void PutAsset(byte[] data, Messages.AssetType type, uint ID, uint CRC)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetAsset(uint ID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Taj/Assets/IAssetManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taj.Messages;
+
+namespace Taj.Assets
+{
+    public interface IAssetManager
+    {
+        void PutAsset(byte[] data, AssetType type, uint ID, uint CRC);
+        byte[] GetAsset(uint ID);
+        byte[] GetAsset(uint CRC);
+    }
+}
